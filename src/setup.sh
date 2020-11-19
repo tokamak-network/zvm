512409fe1e6b505d8620bdc60feca558e2c774f2
Yun <58709201+pleiades-s@users.noreply.github.com>
2020-11-20 00:22:42 +0900

    Delete setup.sh

--- src/setup.sh
+++
@@ -1,6 +0,0 @@
-snarkjs powersoftau new bn128 12 pot12_0000.ptau -v
-snarkjs powersoftau contribute pot12_0000.ptau pot12_0001.ptau --name="First contribution" -v -e="some random text"
-snarkjs powersoftau contribute pot12_0001.ptau pot12_0002.ptau --name="Second contribution" -v -e="some random text"
-snarkjs powersoftau verify pot12_0002.ptau
-snarkjs powersoftau beacon pot12_0002.ptau pot12_beacon.ptau 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon"
-snarkjs powersoftau prepare phase2 pot12_beacon.ptau pot12_final.ptau -v
